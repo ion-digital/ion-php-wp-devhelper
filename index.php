96c17acfed5c536a77b04083e070dc513c7ce584
jmeyer-vlsystems <92323513+jmeyer-vlsystems@users.noreply.github.com>
2024-01-14 16:12:01 +0200

    CD update

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 
  */
 
+require_once __DIR__ . DIRECTORY_SEPARATOR . "vendor/autoload.php";
+
 \Ion\Package::create("ion", "wp-devhelper", function($package) {
 
     $loader = \Ion\Autoloading\Autoloader::create(
