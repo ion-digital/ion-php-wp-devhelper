84893b825bb7639b3465d78f2ba2fd9e7b4f9c93
jmeyer-vlsystems <92323513+jmeyer-vlsystems@users.noreply.github.com>
2024-01-15 19:32:39 +0200

    WIP

--- index.php
+++ index.php
@@ -20,52 +20,49 @@
 if(!empty($bootstrap))
     require_once($bootstrap);
 
-if(class_exists("\\Ion\\Package")) {
+\Ion\Package::create("ion", "wp-devhelper", function($package) {
 
-    \Ion\Package::create("ion", "wp-devhelper", function($package) {
-
-        $loader = \Ion\Autoloading\Autoloader::create(
-            
-            $package, 
-            [ 
-                "source/classes",
-                "source/interfaces",
-                "source/traits"
-            ], 
-            [
-                "builds/" . PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION,
-                "builds/" . PHP_MAJOR_VERSION,
-            ]
-        );
-
-        if (defined("ABSPATH")) {
-
-            require_once( ABSPATH . 'wp-admin' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'upgrade.php' );
-            
-            \Ion\WordPress\WordPressHelper::createContext('ion', 'wp-devhelper', __FILE__, __DIR__, [])
-                    
-                ->initialize(function($context) {
-
-                    // empty for now!
-                })
-            
-                ->activate(function($context) {
-            
-                    // empty for now!
-                })
-                
-                ->deactivate(function($context) {
-            
-                    // empty for now!
-                })
-            
-                ->uninstall(null)
-            
-            ->finalize();    
-        }
-
-        return $loader;
+    $loader = \Ion\Autoloading\Autoloader::create(
         
-    }, __FILE__);
+        $package, 
+        [ 
+            "source/classes",
+            "source/interfaces",
+            "source/traits"
+        ], 
+        [
+            "builds/" . PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION,
+            "builds/" . PHP_MAJOR_VERSION,
+        ]
+    );
 
-}
+    if (defined("ABSPATH")) {
+
+        require_once( ABSPATH . 'wp-admin' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'upgrade.php' );
+        
+        \Ion\WordPress\WordPressHelper::createContext('ion', 'wp-devhelper', __FILE__, __DIR__, [])
+                
+            ->initialize(function($context) {
+
+                // empty for now!
+            })
+        
+            ->activate(function($context) {
+        
+                // empty for now!
+            })
+            
+            ->deactivate(function($context) {
+        
+                // empty for now!
+            })
+        
+            ->uninstall(null)
+        
+        ->finalize();    
+    }
+
+    return $loader;
+    
+}, __FILE__);
+
